bd451113ba54d2b85254145b77c8fdbe3ed31092
Jeff Tran <j.tran@utexas.edu>
2012-11-02 13:12:35 -0500

    Fixed some exception throwing logic for cases where there is no internet connection.

--- Routy/src/org/routy/service/InternetService.java
+++ Routy/src/org/routy/service/InternetService.java
@@ -7,26 +7,39 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.routy.exception.NoInternetConnectionException;
+
 public class InternetService {
 
 	private static final String TAG = "InternetService";
 	
 	
-	public static String getJSONResponse(String url) throws MalformedURLException, IOException {
+	public static String getJSONResponse(String url) throws MalformedURLException, IOException, NoInternetConnectionException {
 		URL distMatUrl = new URL(url.toString());
-		URLConnection conn = distMatUrl.openConnection();
-		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		StringBuffer jsonResp = new StringBuffer();
 		
+		URLConnection conn = null;
 		try {
-			String line = null;
-			while ((line = in.readLine()) != null) {
-				jsonResp.append(line);
-			}
+			conn = distMatUrl.openConnection();
+		} catch (IOException e) {
+			throw new NoInternetConnectionException("Could not get connection to URL: " + distMatUrl.toExternalForm());
+		}
+		
+		if (conn != null) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			StringBuffer jsonResp = new StringBuffer();
 			
-			return jsonResp.toString();
-		} finally {
-			in.close();
+			try {
+				String line = null;
+				while ((line = in.readLine()) != null) {
+					jsonResp.append(line);
+				}
+				
+				return jsonResp.toString();
+			} finally {
+				in.close();
+			}
+		} else {
+			throw new NoInternetConnectionException("Could not get connection to URL: " + distMatUrl.toExternalForm());
 		}
 	}
 }
