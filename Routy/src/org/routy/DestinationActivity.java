822d01699050804dc4ce0da5f4e179e254d80007
Jeff Tran <j.tran@utexas.edu>
2012-11-06 22:00:51 -0600

    Dynamic add/remove destinations working WITH error highlighting.
    Added back all the logic that was in the old DestinationActivity.

--- Routy/src/org/routy/DestinationActivity.java
+++ Routy/src/org/routy/DestinationActivity.java
@@ -2,14 +2,20 @@ package org.routy;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
+
+import junit.framework.Assert;
 
 import org.routy.exception.AmbiguousAddressException;
 import org.routy.exception.NoInternetConnectionException;
+import org.routy.model.AppProperties;
 import org.routy.model.Route;
 import org.routy.model.RouteRequest;
 import org.routy.service.AddressService;
 import org.routy.task.CalculateRouteTask;
+import org.routy.view.DestinationAddView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -17,190 +23,140 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
-import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DestinationActivity extends FragmentActivity {
 	
-	private static final int NUM_DEFAULT_DESTINATIONS = 3;
-	Context mContext;
-	Address originAddress;
-	ArrayList<EditText> destinationEditTexts 	= new ArrayList<EditText>();
-	ArrayList<Button> removeDestinationButtons 	= new ArrayList<Button>();
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_destination);
-        
-        // TODO: should we put 'this.' in front of all class vars used?
-        mContext		= this;
-        Bundle extras 	= getIntent().getExtras();
-        if (extras != null) {
-            originAddress = (Address) extras.get("origin");
-            assert originAddress != null;
-        }
-        
-
-        // instantiate edit texts and their 'remove' buttons
-        for (int x = 0; x < NUM_DEFAULT_DESTINATIONS; x++){
-        	addDestination();
-        }
-        
-        // "Test defaults"
-        Button buttonTestDefaults = (Button) findViewById(R.id.button_test_defaults);
-        buttonTestDefaults.setOnClickListener(listenerTestDefaults);
-        
-        
-        // "Add another destination"
-        Button buttonAddDestination = (Button) findViewById(R.id.button_destination_add);
-        buttonAddDestination.setOnClickListener(listenerAddDestination);
-        
-        // "Go back and choose another origin"
-        	// goes back to OriginActivity
-        
-        // "Route it!"
-        Button buttonRouteIt = (Button) findViewById(R.id.button_route_it);
-        buttonRouteIt.setOnClickListener(listenerRouteIt);
-        
-    }
-    
-    
-	/*
-	 * Click listener for the "Remove" button
-	 */
-    View.OnClickListener listenerTestDefaults = new View.OnClickListener() {
-        public void onClick(View v) {
-        	TableLayout table = (TableLayout) findViewById(R.id.TableLayout_destinations);
-        	TableRow row;
-        	EditText et;
-        	row = (TableRow) table.getChildAt(0);
-        	et = (EditText) row.getChildAt(0);
-        	et.setText(R.string.test_destination_1);
-        	row = (TableRow) table.getChildAt(1);
-        	et = (EditText) row.getChildAt(0);
-        	et.setText(R.string.test_destination_2);
-        	row = (TableRow) table.getChildAt(2);
-        	et = (EditText) row.getChildAt(0);
-        	et.setText(R.string.test_destination_3);
-        }
-    };
-    
-
-	/*
-	 * Click listener for the "Remove" button
-	 */
-    View.OnClickListener listenerRemoveDestination = new View.OnClickListener() {
-        public void onClick(View v) {
-        	TableLayout table = (TableLayout) findViewById(R.id.TableLayout_destinations);
-        	TableRow tablerow = (TableRow) v.getParent();
-        	
-        	table.removeView(tablerow);
-        }
-    };
-    
-    
-	/*
-	 * Click listener for the "Add another destination" button
-	 */
-    View.OnClickListener listenerAddDestination = new View.OnClickListener() {
-        public void onClick(View v) {
-        	addDestination();
-        }
-    };
-    
-    private void addDestination() {
-    	// http://forum.codecall.net/topic/70354-tablelayout-programatically/#axzz2B6OUEzdn
-    	
-    	TableLayout table 	= (TableLayout) findViewById(R.id.TableLayout_destinations);
-        TableRow row		= new TableRow(mContext);
-		EditText et 		= new EditText(mContext);
-		Button b 			= new Button(mContext);
-		int newRowIndex		= table.getChildCount();
+	
+	private final String TAG = "DestinationActivity";
+	
+	private Context mContext;
+	private Address origin;
+	private LinearLayout destLayout;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
-		// Instantiate EditText and Button objects
-		et.setText(getResources().getString(R.string.destination_enter));
-		et.setOnFocusChangeListener((OnFocusChangeListener) listenerDestinationText);
-		b.setText(getResources().getString(R.string.destination_remove));	
+		setContentView(R.layout.activity_destination);
 		
-		// EditTexts and Buttons will have same ID if they're on the same row
-		et.setId(newRowIndex);
-		b.setId(newRowIndex);
+		mContext = this;
 		
-		et.setGravity(Gravity.LEFT);
-		b.setGravity(Gravity.RIGHT);
-
-		b.setOnClickListener(listenerRemoveDestination);
-		
-		destinationEditTexts.add(et);
-		removeDestinationButtons.add(b);
-		
-		// Add et and b columns
-		row.addView(et);
-		row.addView(b);
-		
-		// Set width of row and add row to table
-		row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-		table.addView(row); 
-	}
-    
-	/*
-	 * Click listener for the "Enter a destination" EditText
-	 */
-    View.OnFocusChangeListener listenerDestinationText = new View.OnFocusChangeListener() {
-		@Override
-		public void onFocusChange(View v, boolean hasFocus) {
-        	EditText et 		= (EditText) v;
-        	String currentText 	= et.getText().toString();
-        	String enterText 	= getResources().getString(R.string.destination_enter);
-        	if ("".equals(currentText))
-        		et.setText(enterText);
-        	else if (enterText.equals(currentText))
-        		et.setText("");
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			origin = (Address) extras.get("origin");
+			Assert.assertNotNull(origin);
+			
+			Log.v(TAG, "Origin address: " + origin.getAddressLine(0));
+			TextView originText = (TextView) findViewById(R.id.textview_destinations_origin);
+			originText.setText("Starting from: " + origin.getAddressLine(0));
 		}
-	};
-    
-
-    // TODO: Clean this up
-	// Click listener for the "Route it!" button
-    View.OnClickListener listenerRouteIt = new View.OnClickListener() {
+		
+		destLayout = (LinearLayout) findViewById(R.id.LinearLayout_destinations);
+		
+		addDestinationAddView();
+		
+		// XXX temp "Test defaults"
+        Button buttonTestDefaults = (Button) findViewById(R.id.button_test_defaults);
+        buttonTestDefaults.setText("Test Default Destinations");
+        buttonTestDefaults.setOnClickListener(listenerTestDefaults);
+	}
+	
+	
+	// XXX temp
+	View.OnClickListener listenerTestDefaults = new View.OnClickListener() {
         public void onClick(View v) {
-        	ArrayList<Address> validatedAddresses = new ArrayList<Address>();
+        	if (destLayout.getChildCount() < 3) {
+        		for (int i = destLayout.getChildCount(); i < 3; i++) {
+        			addDestinationAddView();
+        		}
+        	}
         	
-        	// Iterates through entered locations and validates them into addresses.
-        	Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
-        	AddressService addressService =  new AddressService(geocoder, false);		// TODO make getting sensor true/false dynamic	
-        	// TODO: "please wait" screen so activity doesn't block.
-    		try {
-    			// gets the destination text from the EditText boxes and tries to validate the strings
-            	for (int i = 0; i < destinationEditTexts.size(); i++){
-					validatedAddresses.add(
-							addressService.getAddressForLocationString(
-									destinationEditTexts.get(i).getText().toString()));
-            	}
-    			Toast.makeText(mContext, getString(R.string.validated), Toast.LENGTH_LONG).show();	// XXX temp
-			} catch (AmbiguousAddressException e) {
-				// TODO error handling - must step out of this OnClick
-				e.printStackTrace();
-			} catch (NoInternetConnectionException e) {
-				// TODO error handling - must step out of this OnClick
-				e.printStackTrace();
-			}
+        	DestinationAddView view = (DestinationAddView) destLayout.getChildAt(0);
+        	((EditText) view.findViewById(R.id.edittext_destination_add)).setText(getResources().getString(R.string.test_destination_1));
+        	
+        	view = (DestinationAddView) destLayout.getChildAt(1);
+        	((EditText) view.findViewById(R.id.edittext_destination_add)).setText(getResources().getString(R.string.test_destination_2));
+        	
+        	view = (DestinationAddView) destLayout.getChildAt(2);
+        	((EditText) view.findViewById(R.id.edittext_destination_add)).setText(getResources().getString(R.string.test_destination_3));
+        }
+    };
+	
+	
+	void addDestinationAddView() {
+		DestinationAddView v = new DestinationAddView(mContext) {
 
-        	
-        	// TODO: fill in last two parameters of RouteService instantiation with user choice
+			@Override
+			public void onRemoveClicked(UUID id) {
+				Log.v(TAG, "Remove DestinationAddView id=" + id);
+				removeDestinationAddView(id);
+			}
+		};
+		
+		destLayout.addView(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+	}
+	
+	
+	void removeDestinationAddView(UUID id) {
+		// Go through the views list and remove the one that has the given UUID
+		
+		// TODO check out views.remove(Object o) and see how it compares objects to find the right one
+		
+		for (int i = 0; i < destLayout.getChildCount(); i++) {
+			if (((DestinationAddView) destLayout.getChildAt(i)).getUUID().equals(id)) {
+				destLayout.removeViewAt(i);
+				break;
+			}
+		}
+	}
+	
+	
+	public void addAnotherDestination(View v) {
+		Log.v(TAG, "Add another destination.");
+		
+		if (destLayout.getChildCount() < AppProperties.NUM_MAX_DESTINATIONS) {
+			addDestinationAddView();
+		}
+	}
+	
+	
+	// TODO Right now it's validating ALL destinations every time (regardless of if any were already validated) -- make it more efficient
+	public void acceptDestinations(View v) {
+		Log.v(TAG, "Validate destinations and calculate route if they're good.");
+		
+		for (int i = 0; i < destLayout.getChildCount(); i++) {
+			Log.v(TAG, "Destination " + i + ": " + ((EditText) ((DestinationAddView) destLayout.getChildAt(i)).findViewById(R.id.edittext_destination_add)).getText().toString());
+		}
+		
+		// Validate the addresses and highlight any errors.
+		List<Address> validatedAddresses = validateDestinations();
+		boolean hasErrors = false;
+		for (int i = 0; i < validatedAddresses.size(); i++) {
+			if (validatedAddresses.get(i) == null) {
+				flagInvalidDestination(i);
+				hasErrors = true;
+			}
+		}
+		
+		if (!hasErrors) {
+			// TODO Calculate route and go to results activity
+			Toast.makeText(mContext, getString(R.string.validated), Toast.LENGTH_LONG).show();	// XXX temp
+			
+			// TODO: fill in last two parameters of RouteService instantiation with user choice
         	// Instantiates a Route object with validated addresses and calls ResultsActivity
         	try {
-				/*RouteService routeService = new RouteService(originAddress, validatedAddresses, RouteOptimizePreference.PREFER_DURATION, false);
-				Route route = routeService.getBestRoute();		// TODO this needs to be done in an AsyncTask (otherwise NetworkOnMainThreadException)*/
+				/*RouteService routeService = new RouteService(origin, validatedAddresses, RouteOptimizePreference.PREFER_DURATION, false);
+				Route route = routeService.getBestRoute();*/
         		
         		CalculateRouteTask task = new CalculateRouteTask() {
 					
@@ -216,19 +172,59 @@ public class DestinationActivity extends FragmentActivity {
 					}
 				};
 				
-				task.execute(new RouteRequest(originAddress, validatedAddresses, false));
+				task.execute(new RouteRequest(origin, validatedAddresses, false));
 				
 			} catch (Exception e) {
 				// TODO error handling
 				e.printStackTrace();
 			}
-          
-        }
-      };
-    
-    @Override
+		}
+	}
+	
+	
+	private void flagInvalidDestination(int position) {
+		if (position >= 0 && position < destLayout.getChildCount()) {
+			DestinationAddView view = (DestinationAddView) destLayout.getChildAt(position);
+			view.setInvalid();
+		}
+	}
+	
+	
+	private List<Address> validateDestinations() {
+		List<Address> addresses = new ArrayList<Address>();
+		
+		// Iterates through entered locations and validates them into addresses.
+    	Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+    	AddressService addressService =  new AddressService(geocoder, false);		// TODO make getting sensor true/false dynamic	
+
+    	// TODO: "please wait" screen so activity doesn't block.
+		DestinationAddView view = null;
+		Address address = null;
+		
+		// gets the destination text from the EditText boxes and tries to validate the strings
+    	for (int i = 0; i < destLayout.getChildCount(); i++){
+			try {
+        		view = (DestinationAddView) destLayout.getChildAt(i);
+        		address = addressService.getAddressForLocationString(((EditText) view.findViewById(R.id.edittext_destination_add)).getText().toString());
+        		addresses.add(address);
+			} catch (AmbiguousAddressException e) {
+				// TODO error handling - must step out of this OnClick
+				Log.v(TAG, "Ambiguous address.");
+				addresses.add(null);
+			} catch (NoInternetConnectionException e) {
+				// TODO error handling - must step out of this OnClick
+				Log.v(TAG, "No internet connection when trying to validate addresses.");
+			}
+    	}
+		
+		return addresses;
+	}
+	
+	
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_destination, menu);
         return true;
     }
+
 }
---
+++ Routy/src/org/routy/view/DestinationAddView.java
@@ -0,0 +1,84 @@
+package org.routy.view;
+
+import java.util.UUID;
+
+import org.routy.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public abstract class DestinationAddView extends LinearLayout {
+
+	
+	private UUID id;
+	private String address;
+	private EditText editText;
+	
+	
+	/**
+	 * Called when the "remove" button is clicked for a DestinationAddView
+	 * @param id		the {@link UUID} of the DestinationAddView to remove
+	 */
+	public abstract void onRemoveClicked(UUID id);
+	
+	
+	public DestinationAddView(Context context) {
+		this(context, "");
+	}
+	
+	public DestinationAddView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		this.id = UUID.randomUUID();
+		this.address = "";
+		
+		initViews(context);
+	}
+	
+	public DestinationAddView(Context context, String address) {
+		super(context);
+		
+		this.id = UUID.randomUUID();
+		this.address = address;
+		
+		initViews(context);
+	}
+	
+	
+	private void initViews(Context context) {
+		// Inflate the view for an "add destination" text field and remove button
+		LayoutInflater inflater = LayoutInflater.from(context);
+		inflater.inflate(R.layout.view_destination_add, this);
+		
+		editText = (EditText) findViewById(R.id.edittext_destination_add);
+		editText.setText(address);
+		
+		Button removeButton = (Button) findViewById(R.id.button_destination_remove);
+		removeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				onRemoveClicked(id);
+			}
+		});
+	}
+	
+	
+	public UUID getUUID() {
+		return this.id;
+	}
+	
+	
+	public void setInvalid() {
+		// TODO do what's necessary visually to show that this destination is invalid
+//		editText.setTextColor(getResources().getColor(R.color.Red));
+		editText.setBackgroundColor(getResources().getColor(R.color.Pink));
+	}
+	
+	
+}
