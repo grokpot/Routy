2616400cb2ce1c3cd214c8facc397708ce3f4f71
Ryan Prater <ryanprater@utexas.edu>
2012-12-01 16:47:55 -0600

    Results are now dynamically inflated.

--- Routy/src/org/routy/ResultsActivity.java
+++ Routy/src/org/routy/ResultsActivity.java
@@ -1,9 +1,13 @@
 package org.routy;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.routy.model.Route;
+import org.routy.view.DestinationRowView;
+import org.routy.view.ResultsSegmentView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
 import android.media.AudioManager;
@@ -14,157 +18,128 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ResultsActivity extends FragmentActivity {
 
+	Context mContext;
+
 	// The Route sent by DestinationActivity
 	Route route;
-	
-	// Segment Labels
-	private TextView segmentTexts[];
-	// Segment buttons
-	private Button segmentButtons[]; 
-	
+
+	private LinearLayout resultsLayout;
+
 	private final String TAG = "ResultsActivity";
-	
-  private SoundPool sounds;
-  private int click;
-  
-  AudioManager audioManager;
-  float volume;
 
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-        volume = (float) audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
-        
-        sounds = new SoundPool(3, AudioManager.STREAM_MUSIC, 0); 
-        click = sounds.load(this, R.raw.routyclick, 1);
-        setContentView(R.layout.activity_results);
-        
-        Bundle extras 	= getIntent().getExtras();
-        if (extras != null) {
-            int distance = (Integer) extras.get("distance");
-            ArrayList<Address> addresses =  (ArrayList<Address>) extras.get("addresses");
-            Log.v(TAG, "Results: " + addresses.size() + " addresses");
-            route = new Route(addresses, distance);
-        }
-        
-        buildResultsView();
-    }
+	private SoundPool sounds;
+	private int click;
 
-    // Dynamically build the results screen by iterating through segments and creating TextViews/Buttons
-    private void buildResultsView() {
-    	int addressesSize	= route.getAddresses().size();
-		segmentTexts 		= new TextView[addressesSize];
-		segmentButtons 		= new Button[addressesSize];
-		LinearLayout resultsLayout = (LinearLayout) findViewById(R.id.layout_results);
-		
-		TextView text_total_distance = (TextView) findViewById(R.id.textview_total_distance);
-		text_total_distance.setText(getString(R.string.total_distance) + ((double) route.getTotalDistance())/1000 + "km");
-		
-		for (int addressIndex = 0; addressIndex < addressesSize; addressIndex++){
-			
-			// Create the TextView for this specific segment
-			segmentTexts[addressIndex] = new TextView(this);
-			
-			// Dynamically set the text for the TextViews
-			
-			// Hacky "if" statement that displays the address if it's not a Google Place
-			String addressText = route.getAddresses().get(addressIndex).getFeatureName();
-//			if (addressText == null || addressText.length() == 0) {
-//				addressText = route.getAddresses().get(addressIndex).getExtras().getString("formatted_address");
-//			}
-			if (route.getAddresses().get(addressIndex).getThoroughfare() != null) {
-				addressText = route.getAddresses().get(addressIndex).getSubThoroughfare();
-				addressText += " " + route.getAddresses().get(addressIndex).getThoroughfare();				
-			}
-			
-			segmentTexts[addressIndex].setText(addressText);
-			
-			// Dynamically position TextView on the screen
-			segmentTexts[addressIndex].layout(0, 20, 0, 0);
-			resultsLayout.addView(segmentTexts[addressIndex]);
-			
-			// If it's not the last address, add a button segment
-			if (addressIndex != (addressesSize - 1)){
-				// Create the Button for this specific segment
-				segmentButtons[addressIndex] = new Button(this);
-				
-				// Dynamically set the text for the Button
-				segmentButtons[addressIndex].setText(getString(R.string.view_segment));
-				segmentButtons[addressIndex].setId(addressIndex);
-				
-				// Instantiate the onClickListener
-				segmentButtons[addressIndex].setOnClickListener(map_segment_listener);
+	AudioManager audioManager;
+	float volume;
 
-				// Dynamically position button on the screen
-				segmentButtons[addressIndex].layout(0, 20, 0, 0);
-				resultsLayout.addView(segmentButtons[addressIndex]);
-			}
-		}	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mContext = this;
+
+		audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+		volume = (float) audioManager
+				.getStreamVolume(AudioManager.STREAM_SYSTEM);
+
+		sounds = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+		click = sounds.load(this, R.raw.routyclick, 1);
+		setContentView(R.layout.activity_results);
+
+		// Get the layout containing the list of destination
+		resultsLayout = (LinearLayout) findViewById(R.id.linearlayout_results);
+
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			int distance = (Integer) extras.get("distance");
+			ArrayList<Address> addresses = (ArrayList<Address>) extras.get("addresses");
+			Log.v(TAG, "Results: " + addresses.size() + " addresses");
+			route = new Route(addresses, distance);
+		}
+
+		buildResultsView();
 	}
 
-    View.OnClickListener map_segment_listener = new View.OnClickListener() {
-		@Override
-    public void onClick(View v) {
-		  volume = (float) audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
-	    volume = volume / audioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM);
-		  sounds.play(click, volume, volume, 1, 0, 1);
-		  
-			// Get the ID assigned in buildResultsView() so we can get the respective segment
-			final int start 	= v.getId();
-			// We need the next address to calculate a segment to send to Google, so we get the next destination index as well.
-			final int dest		= start + 1;
-			assert route.getAddresses().get(dest) != null;
-			
-			// Build call to Google Maps native app
-			double startingAddressLat 		= route.getAddresses().get(start).getLatitude();
-			double startingAddressLong 		= route.getAddresses().get(start).getLongitude();
-			double destinationAddressLat 	= route.getAddresses().get(dest).getLatitude();
-			double destinationAddressLong 	= route.getAddresses().get(dest).getLongitude();
-			String mapsCall = "http://maps.google.com/maps?saddr=" 
-					+ startingAddressLat + ","
-					+ startingAddressLong + "&daddr="
-					+ destinationAddressLat + ","
-					+ destinationAddressLong;
-			Log.d(TAG, "maps segment call URI: " + mapsCall);
-			
-			// Open Google Maps App on the device
-			Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(mapsCall));
-			startActivity(intent);
-		}
-	};
-    
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_results, menu);
-        return true;
-    }
-	
-  @Override
-  protected void onResume() {   
-     super.onResume(); 
-     
-     sounds = new SoundPool(3, AudioManager.STREAM_MUSIC, 0); 
+	// Dynamically build the results screen by building a ResultsRowView, which inflates view_result_segment
+	private void buildResultsView() {
+		int addressesSize = route.getAddresses().size();
 
-     click = sounds.load(this, R.raw.routyclick, 1);
-   }
-  
-  
-  @Override
-  public void onPause() {
-    super.onPause();
-    
-    if(sounds != null) { 
-      sounds.release(); 
-      sounds = null; 
-    } 
-  }
+		Address address = null;
+		// TODO: do we need lastAddress?
+		boolean isLastAddress = false;
+		for (int addressIndex = 0; addressIndex < addressesSize; addressIndex++) {
+			address = route.getAddresses().get(addressIndex);
+			// special case if it's the last segment
+			if (addressIndex == addressesSize - 1) {
+				isLastAddress = true;
+			}
+
+			// TODO: do we need to send addressIndex?
+			ResultsSegmentView v = new ResultsSegmentView(mContext, address, addressIndex, isLastAddress) {
+
+				@Override
+				public void onSegmentClicked(int id, boolean isLastAddress) {
+					if (!isLastAddress){
+						volume = (float) audioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
+						volume = volume / audioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM);
+						sounds.play(click, volume, volume, 1, 0, 1);
+	
+						// Get start and end Addresses from route[] - the index is the id in ResultsSegmentView
+						Address startAddress	= route.getAddresses().get(id);
+						Address endAddress 		= route.getAddresses().get(id + 1);
+	
+						// Build call to Google Maps native app
+						double startAddressLat = startAddress.getLatitude();
+						double startAddressLong = startAddress.getLongitude();
+						double endAddressLat = endAddress.getLatitude();
+						double endAddressLong = endAddress.getLongitude();
+						String mapsCall = "http://maps.google.com/maps?saddr="
+								+ startAddressLat + "," + startAddressLong
+								+ "&daddr=" + endAddressLat + ","
+								+ endAddressLong;
+						Log.d(TAG, "maps segment call URI: " + mapsCall);
+	
+						// Open Google Maps App on the device
+						Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(mapsCall));
+						startActivity(intent);
+					}
+				}
+			};
+
+			resultsLayout.addView(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		}
+
+		TextView text_total_distance = (TextView) findViewById(R.id.textview_total_distance);
+		text_total_distance.setText(getString(R.string.total_distance) + ((double) route.getTotalDistance()) / 1000 + "km");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_results, menu);
+		return true;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sounds = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+		click = sounds.load(this, R.raw.routyclick, 1);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (sounds != null) {
+			sounds.release();
+			sounds = null;
+		}
+	}
 }
