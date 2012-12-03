59802d61eb6035e5410fb8341c63127f897e457e
Jeff Tran <j.tran@utexas.edu>
2012-12-02 21:19:20 -0600

    In sync with remote issue51 branch.

--- Routy/src/org/routy/ResultsActivity.java
+++ Routy/src/org/routy/ResultsActivity.java
@@ -2,9 +2,11 @@ package org.routy;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.routy.fragment.OneButtonDialog;
 import org.routy.model.Route;
+import org.routy.view.DestinationRowView;
 import org.routy.view.ResultsSegmentView;
 
 import android.app.AlertDialog;
@@ -38,17 +40,12 @@ public class ResultsActivity extends MapActivity {
 	private FragmentActivity context;
 	Context mContext;
 
+	private MapView mapView;
+	
 	private SharedPreferences resultsActivityPrefs;
 	
 	// The Route sent by DestinationActivity
 	Route route;
-	
-	private MapView mapView;
-
-	// Segment Labels
-	private TextView segmentTexts[];
-	// Segment buttons
-	private Button segmentButtons[]; 
 
 	private LinearLayout resultsLayout;
 
@@ -87,44 +84,46 @@ public class ResultsActivity extends MapActivity {
 
 		buildResultsView();
 		
-		resultsActivityPrefs = getSharedPreferences("results_prefs", MODE_PRIVATE);
-		// TODO: for testing purposes. Remove before prod.
-		showNoobDialog();
-		// First-time user dialog cookie
-		boolean noobCookie = resultsActivityPrefs.getBoolean("noob_cookie", false);
-		if (!noobCookie){
-			showNoobDialog();
-			userAintANoob();
-		}
+		initMapView();
+		
+//		resultsActivityPrefs = getSharedPreferences("results_prefs", MODE_PRIVATE);
+//		// TODO: for testing purposes. Remove before prod.
+//		showNoobDialog();
+//		// First-time user dialog cookie
+//		boolean noobCookie = resultsActivityPrefs.getBoolean("noob_cookie", false);
+//		if (!noobCookie){
+//			showNoobDialog();
+//			userAintANoob();
+//		}
 	}
 	
 	
-	/**
-	 * Displays an {@link AlertDialog} with one button that dismisses the dialog. Dialog displays helpful first-time info.
-	 * 
-	 * @param message
-	 */
-	private void showNoobDialog() {
-		OneButtonDialog dialog = new OneButtonDialog(getResources().getString(R.string.results_noob_title), getResources().getString(R.string.results_noob_instructions)) {
-			@Override
-			public void onButtonClicked(DialogInterface dialog, int which) {
-				dialog.dismiss();
-			}
-
-		};
-		dialog.show(context.getSupportFragmentManager(), TAG);
-	}
+//	/**
+//	 * Displays an {@link AlertDialog} with one button that dismisses the dialog. Dialog displays helpful first-time info.
+//	 * 
+//	 * @param message
+//	 */
+//	private void showNoobDialog() {
+//		OneButtonDialog dialog = new OneButtonDialog(getResources().getString(R.string.results_noob_title), getResources().getString(R.string.results_noob_instructions)) {
+//			@Override
+//			public void onButtonClicked(DialogInterface dialog, int which) {
+//				dialog.dismiss();
+//			}
+//		};
+//		dialog.show(getSupportFragmentManager(), TAG);
+//	}
+//	
+//	/**
+//	 *  If the user sees the first-time instruction dialog, they won't see it again next time.
+//	 */
+//	private void userAintANoob() {
+//		SharedPreferences.Editor ed = resultsActivityPrefs.edit();
+//		ed.putBoolean("noob_cookie", true);
+//		ed.commit();	
+//	}
 	
-	/**
-	 *  If the user sees the first-time instruction dialog, they won't see it again next time.
-	 */
-	private void userAintANoob() {
-		SharedPreferences.Editor ed = resultsActivityPrefs.edit();
-		ed.putBoolean("noob_cookie", true);
-		ed.commit();	
-	}
 	
-	/*void initMapView() {
+	void initMapView() {
 		mapView = (MapView) findViewById(R.id.mapview_results);
 		mapView.setBuiltInZoomControls(false);		// Don't let the user do anything to the map, and don't display zoom buttons
 		
@@ -132,36 +131,8 @@ public class ResultsActivity extends MapActivity {
 		// TODO instead of doing this, calculate the span of lat and lng and call zoomToSpan
 		controller.setCenter(new GeoPoint(30390960, -97697490));
 		controller.setZoom(17);
-	}*/
-
-	View.OnClickListener map_segment_listener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			sounds.play(click, 1, 1, 1, 0, 1);
-
-			// Get the ID assigned in buildResultsView() so we can get the respective segment
-			final int start 	= v.getId();
-			// We need the next address to calculate a segment to send to Google, so we get the next destination index as well.
-			final int dest		= start + 1;
-			assert route.getAddresses().get(dest) != null;
-
-			// Build call to Google Maps native app
-			double startingAddressLat 		= route.getAddresses().get(start).getLatitude();
-			double startingAddressLong 		= route.getAddresses().get(start).getLongitude();
-			double destinationAddressLat 	= route.getAddresses().get(dest).getLatitude();
-			double destinationAddressLong 	= route.getAddresses().get(dest).getLongitude();
-			String mapsCall = "http://maps.google.com/maps?saddr=" 
-					+ startingAddressLat + ","
-					+ startingAddressLong + "&daddr="
-					+ destinationAddressLat + ","
-					+ destinationAddressLong;
-			Log.d(TAG, "maps segment call URI: " + mapsCall);
-
-			// Open Google Maps App on the device
-			Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(mapsCall));
-			startActivity(intent);
-		}
-	};
+	}
+	
 
 	// Dynamically build the results screen by building a ResultsRowView, which inflates view_result_segment
 	private void buildResultsView() {
@@ -223,6 +194,7 @@ public class ResultsActivity extends MapActivity {
 		return new DecimalFormat("#.##").format(distanceInMiles);
 	}
 
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_results, menu);
@@ -244,7 +216,7 @@ public class ResultsActivity extends MapActivity {
 			sounds = null;
 		}
 	}
-	
+
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
