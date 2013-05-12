72d8de8e950afe9c5add9a76a04221b007b14318
Ryan Prater <rprater@thinktiv.com>
2013-05-12 12:52:59 -0500

    Added About dialog to preferencesfragment

---
+++ Routy/src/org/routy/fragment/PreferencesFragment.java
@@ -0,0 +1,37 @@
+package org.routy.fragment;
+
+import org.routy.R;
+
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceFragment;
+
+public class PreferencesFragment extends PreferenceFragment {
+	private static final String TAG = "PreferenceFragment";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+	    super.onCreate(savedInstanceState);
+	    final FragmentManager fragmentManager = getFragmentManager();;
+	    addPreferencesFromResource(R.xml.preferences);
+	    
+	    Preference myPref = (Preference) findPreference("pref_about");
+	    myPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				OneButtonDialog dialog = new OneButtonDialog(getResources().getString(R.string.origin_noob_title), getResources().getString(R.string.origin_noob_instructions)) {
+					@Override
+					public void onButtonClicked(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				};
+				dialog.show(fragmentManager, TAG);
+				return true;
+			}
+	    });
+	}
+}
